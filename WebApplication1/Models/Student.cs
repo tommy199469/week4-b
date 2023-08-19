cd4cacd0717dea01d5c2df3f919d41db1fb2f6f2
thomas <thomasng.discord@gmail.com>
2023-08-20 00:22:21 +0800

    finish

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+    }
+
+}
